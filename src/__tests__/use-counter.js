e9e0307d7d8e81e1926be455ebcc41c76c1da602
Aleksandr Antropov <aleksander.antropov@gmail.com>
2023-03-06 18:45:13 +0300

    update use-counter

--- src/__tests__/use-counter.js
+++ src/__tests__/use-counter.js
@@ -26,3 +26,16 @@ test('allows customization of step', () => {
   act(() => result.current.decrement());
   expect(result.current.count).toBe(0);
 });
+
+test('the step can be changed', () => {
+  const { result, rerender } = renderHook(useCounter, {
+    initialProps: { step: 2 },
+  });
+
+  expect(result.current.count).toBe(0);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(2);
+  rerender({ step: 1 });
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(1);
+});
